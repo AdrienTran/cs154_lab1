96f43978c1a2dbdbde7f7b833b40113309bd56c7
Adrien Tran <adrien.tran600@gmail.com>
2025-01-10 14:10:08 -0800

    finished clock

--- ucsbcs154lab1_clock.py
+++ ucsbcs154lab1_clock.py
@@ -19,27 +19,43 @@ output_minutes = pyrtl.Output(bitwidth=6, name="output_minutes") # name="output_
 output_hours = pyrtl.Output(bitwidth=5, name="output_hours")   # name="output_hours"
 
 # Initialize Registers Here
-seconds = pyrtl.Register(bitwidth=6, name='s')
+seconds = pyrtl.Register(bitwidth=6, name='s', reset_value=1)
 minutes = pyrtl.Register(bitwidth=6, name='m')
 hours = pyrtl.Register(bitwidth=5, name='h')
 
 # Put Sequential Logic Here
 # update the seconds register here
 with pyrtl.conditional_assignment:
-    ...
+    with seconds == 59:
+        seconds.next |= 0
+    with pyrtl.otherwise:
+        seconds.next |= seconds + 1
+    
+    
 
 
 # update the minutes register here
 with pyrtl.conditional_assignment:
-    ...
+    with seconds == 59:
+        with minutes == 59:
+            minutes.next |= 0
+        with pyrtl.otherwise:
+            minutes.next |= minutes + 1
 
 # update the hours register here
 with pyrtl.conditional_assignment:
-    ...
+    with minutes == 59:
+        with seconds == 59:
+            with hours == 24:
+                hours.next |= 0
+            with pyrtl.otherwise:
+                hours.next |= hours + 1
 
 
 # Assign your outputs here
-
+output_seconds <<= seconds
+output_minutes <<= minutes
+output_hours <<= hours
 
 
 # Simulation Code
@@ -54,7 +70,7 @@ if __name__ == "__main__":
     # sim_trace.render_trace(repr_func=str)
 
     ### Uncomment the following lines to see the most recent values of hours, minutes, and seconds
-    # print("hours:", sim_trace.trace["output_hours"][-1])
-    # print("minutes:", sim_trace.trace["output_minutes"][-1])
-    # print("seconds:", sim_trace.trace["output_seconds"][-1])
+    print("hours:", sim_trace.trace["output_hours"][-1])
+    print("minutes:", sim_trace.trace["output_minutes"][-1])
+    print("seconds:", sim_trace.trace["output_seconds"][-1])
 
